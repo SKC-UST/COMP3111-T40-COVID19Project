15cf36b6eb49bd880f30d2465ae7ee4c9a7c41c1
calmChowder3 <ytc314a@gmail.com>
2021-11-05 23:30:14 +0800

    changed Database CSV reader to opencsv

--- src/main/java/comp3111/covid/datastorage/Database.java
+++ src/main/java/comp3111/covid/datastorage/Database.java
@@ -8,13 +8,21 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Database {
 	
 	private class LocationData {
-		private String isoCode;
-		private String continent;
-		private String location;
+		private String locationIsoCode;
+		private String locationContinent;
+		private String locationName;
+		private ArrayList<DayData> dataList = new ArrayList();
 		
 		private class DayData {
 			//Represents a row in the CSV file
@@ -22,48 +30,47 @@ public class Database {
 			private int newCases;
 			private int newDeaths;
 			private int newVaccinations;
+			
+			DayData(String date, String caseNum, String deathsNum, String vcaccinationNum){
+				try {
+					this.dataDate = new SimpleDateFormat("d/MM/yyyy").parse(date);
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				this.newDeaths = Integer.parseInt(caseNum);
+				this.newCases = Integer.parseInt(deathsNum);
+				this.newVaccinations = Integer.parseInt(vcaccinationNum);
+			}
+		}
+		
+		LocationData(String isoCode, String continent, String location) {
+			this.locationIsoCode = isoCode;
+			this.locationContinent = continent;
+			this.locationName = location;
 		}
 	}
 	
-	private ArrayList<ArrayList<String>> arrayStorage = new ArrayList<ArrayList<String>>();
+	private List<String[]> arrayStorage;
+	private HashMap<String, LocationData> hashStorage = new HashMap<String, LocationData>(); //isoCode as key
 	private boolean datasetPresent = false;
-	private HashMap<String, LocationData> hashStorage = new HashMap<String, LocationData>();
 	
 	public void importCSV(File csvDataset) {
-		//TODO: skip the first line (may consider using CSV reader)
-		String line = "";
-    	try {
-			BufferedReader br = new BufferedReader(new FileReader(csvDataset));
-			int index = 0;
-			
-			while((line = br.readLine()) != null) {
-				arrayStorage.add(new ArrayList<String>());
-				String[] values = line.split(",");
-				for(String value : values) {
-					arrayStorage.get(index).add(value);
-				}
-				++index;
-			}
-			br.close();
-		} 
-    	catch (FileNotFoundException e) {
+		try {
+			CSVReader csvReader = new CSVReaderBuilder(new FileReader(csvDataset)).withSkipLines(1).build();
+			this.arrayStorage = csvReader.readAll();
+		}
+		catch(Exception e) {
 			e.printStackTrace();
 		}
-    	catch (IOException e) {
-    		e.printStackTrace();
-    	}
-    	datasetPresent = true;
-	}
-	
-	public void removeDataset(){
-		datasetPresent = false;
-		for(int i = 0; i < arrayStorage.size(); ++i) {
-			arrayStorage.get(i).clear();
-		}
-		arrayStorage.clear();
 	}
 	
 	public void printDatabaseContent() {
-		System.out.println(arrayStorage.get(1));
+		for(String[] row : this.arrayStorage) {
+			for(String cell : row) {
+				System.out.print(cell + "\t");
+			}
+			System.out.println();
+		}
 	}
 }
