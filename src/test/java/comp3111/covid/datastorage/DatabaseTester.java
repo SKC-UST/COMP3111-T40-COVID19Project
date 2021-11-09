54dc3a98d5cec099c1b18674abfb69a7549cda22
skchoi-github <skchoiab@connect.ust.hk>
2021-11-10 00:32:09 +0800

    Table sample (JTable) + line chart sample (the sample class)

--- src/test/java/comp3111/covid/datastorage/DatabaseTester.java
+++ src/test/java/comp3111/covid/datastorage/DatabaseTester.java
@@ -1,9 +1,11 @@
 package comp3111.covid.datastorage;
 
 import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
 import java.util.Date;
 import java.text.SimpleDateFormat; 
 
@@ -15,18 +17,46 @@ import comp3111.covid.datastorage.Database.DataTitle;
  
 
 public class DatabaseTester {
-	Database database = new Database();
-	File file = new File("COVID_Dataset_v1.0.csv");
+	Database database;
+	File csvFile;
+	private static final double DELTA = 1e-15;
 	
 	@Before
 	public void setUp() throws Exception {
-		database.importCSV(file);
+		database = new Database();
+		this.csvFile = new File("./src/main/resources/dataset/COVID_Dataset_v1.0.csv");
+		database.importCSV(csvFile);
 	}
 
 	@Test
-	public void searchTotalDataNoResult() {
-	    String sDate1="31/12/1998";  
-	    Date date1=new SimpleDateFormat("dd/MM/yyyy").parse(sDate1);  
-		assertEquals(-1, Database.searchTotalData("HKG", date1, DataTitle.CASE));
+	public void searchTotalDataResult() {
+	    String sDate1="2/24/2020";  
+	    String sDate2="01/01/1970"; 
+	    try 
+	    {
+		    Date date1=new SimpleDateFormat("MM/dd/yyyy").parse(sDate1);  
+		    Date date2=new SimpleDateFormat("MM/dd/yyyy").parse(sDate2); 
+			assertEquals(1, database.searchTotalData("AFG", date1, DataTitle.CASE));
+			assertEquals(-1, database.searchTotalData("AFG", date2, DataTitle.CASE));
+	    }
+
+		catch (Exception e) 
+	    {
+			 e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void searchRateDataResult() {
+	    String sDate1="2/24/2020";  
+	    try 
+	    {
+		    Date date1=new SimpleDateFormat("MM/dd/yyyy").parse(sDate1);   
+			assertEquals(0.026, database.searchRateData("AFG", date1, DataTitle.CASE), DELTA);
+	    }
+		catch (Exception e) 
+	    {
+			 e.printStackTrace();
+		}
 	}
 }
