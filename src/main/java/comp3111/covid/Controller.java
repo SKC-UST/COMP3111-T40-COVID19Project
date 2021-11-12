a3fad6f40f0b38f4a03131f88f1a1551a1b2d9a1
calmChowder3 <ytc314a@gmail.com>
2021-11-12 12:51:05 +0800

    implementation using separate fxml and controllers for tabs

--- src/main/java/comp3111/covid/Controller.java
+++ src/main/java/comp3111/covid/Controller.java
@@ -12,6 +12,7 @@ import com.sun.prism.paint.Color;
 
 import comp3111.covid.datastorage.Database;
 import comp3111.covid.datastorage.Database.DataTitle;
+import comp3111.covid.tabs.TabC3Controller;
 
 import java.awt.BorderLayout;
 import java.awt.Font;
@@ -155,8 +156,11 @@ public class Controller {
 
     @FXML
     private TextArea textAreaConsole;
-
-    private static final Database database = new Database();
+    
+    @FXML private Tab tabC3;
+    @FXML private TabC3Controller tabC3Controller;
+    private Context context = Context.getInstance();
+    private Database database = context.getDatabase();
   
 
     /**
@@ -285,10 +289,14 @@ public class Controller {
     	File csvFile = chooseCSVFile();
     	// TODO: handle file not chosen
     	if(csvFile != null) {
-    		Controller.database.importCSV(csvFile);
+    		this.database.importCSV(csvFile);
         	System.out.println("successfully imported");
-        	Controller.database.printDatabaseContent();
+        	this.database.printDatabaseContent();
     	}
+    }
+    
+    Database getDatabase() {
+    	return this.database;
     }
 }
 
