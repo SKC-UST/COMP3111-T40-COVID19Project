675e6f2284a5e944a9a220079ac16bf88ab8e1e1
calmChowder3 <ytc314a@gmail.com>
2021-11-15 16:57:31 +0800

    created superclass TabsController

---
+++ src/main/java/comp3111/covid/tabs/TabController.java
@@ -0,0 +1,51 @@
+package comp3111.covid.tabs;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.util.ArrayList;
+
+import org.controlsfx.control.CheckListView;
+
+import comp3111.covid.Context;
+import comp3111.covid.datastorage.Database;
+
+
+public class TabController {
+	private Context context = Context.getInstance();
+	private Database database = context.getDatabase();
+	private ArrayList<String> selectedLocations = new ArrayList<String>();
+
+    @FXML
+    private Button confirmButton;
+
+    @FXML
+    private CheckListView<String> locationsCheckboxList;
+    
+    public void updateCheckboxList() {
+    	
+    	System.out.println("update");
+    	
+    	ArrayList<String> locations = database.getLocationNames();
+		final ObservableList<String> locationList = FXCollections.observableArrayList();
+		for(String location : locations) {
+			locationList.add(location);
+		}
+		locationsCheckboxList.setItems(locationList);
+		
+		this.locationsCheckboxList.getCheckModel().getCheckedItems().addListener(new ListChangeListener<String>() {
+			public void onChanged(ListChangeListener.Change<? extends String> c) {
+				System.out.println(locationsCheckboxList.getCheckModel().getCheckedItems());
+			}
+		});
+    }
+    
+    @FXML
+    void handleConfirmSelection(ActionEvent event) {
+
+    }
+}
