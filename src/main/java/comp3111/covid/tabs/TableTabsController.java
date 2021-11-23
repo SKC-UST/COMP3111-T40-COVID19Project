939642855f9450ebcf550f8a17cecac9e00ca8c2
calmChowder3 <ytc314a@gmail.com>
2021-11-23 16:59:40 +0800

    completed TableTabsController documentation

--- src/main/java/comp3111/covid/tabs/TableTabsController.java
+++ src/main/java/comp3111/covid/tabs/TableTabsController.java
@@ -20,22 +20,28 @@ import java.util.ArrayList;
 import comp3111.covid.datastorage.Database;
 
 /**
- * place holder
- * @author ytc314
- *
+ * This is an abstract class representing the controllers of all the tabs that displays tables.
+ * This class is inherited by TabA1pageController, TabB1pageController, and TabC1pageController
  */
-public class TableTabsController extends TabController {
+public abstract class TableTabsController extends TabController {
 	@FXML private DatePicker datePicker;
 	@FXML private Button confirmButton;
-	@FXML protected TableColumn<TableView<TableData>,String> countryCol;
-	@FXML protected TableColumn<TableView<TableData>,String> totalCol;
-	@FXML protected TableColumn<TableView<TableData>,String> rateCol;
-	@FXML protected TableView<TableData> dataTable;
+	@FXML private TableColumn<TableView<TableData>,String> countryCol;
+	@FXML private TableColumn<TableView<TableData>,String> totalCol;
+	@FXML private TableColumn<TableView<TableData>,String> rateCol;
+	@FXML private TableView<TableData> dataTable;
 	@FXML protected Label tableTitlelbl;
-	
+	/**
+	 * The target date chosen by the user.
+	 * Updated whenever the user changes the value in the datepicker.
+	 */
 	protected LocalDate selectedDate = null;
 	
 	@Override
+	/**
+	 * {@inheritDoc}
+	 * TableTabsController, this method is also responsible for setting {@link javafx.scene.control.cell.PropertyValueFactory} in each {@link javafx.scene.control.TableColumn} in the table.
+	 */
 	public void initialize() {
 		super.initialize();
 		
@@ -43,7 +49,15 @@ public class TableTabsController extends TabController {
 		this.totalCol.setCellValueFactory(new PropertyValueFactory<TableView<TableData>, String>("totalData"));
 		this.rateCol.setCellValueFactory(new PropertyValueFactory<TableView<TableData>, String>("rateData"));
 	}
-	
+	/**
+     * This method handles user input errors.
+     * It handles 3 types of errors:
+     * 1. No Country is chosen.
+     * 2. No date is selected.
+     * 3. The the target date is out of the range of the chosen dataset.
+     * A pop-up window with the corresponding message will remind the user of the error.
+     * @return a negative integer error code if an error occurs, 0 if no error.
+     */
 	protected int handleTableError() {
 		if(this.getSelectedIso().isEmpty()) {
 			this.handleError("Please Choose at Least One Country!", "Country Input Error");
@@ -62,7 +76,9 @@ public class TableTabsController extends TabController {
 		}
 		return 0;
 	}
-	
+	/**
+	 * A class holding data to be put into the table.
+	 */
 	public class TableData {
 		private final SimpleStringProperty countryName;
 		private final SimpleStringProperty totalData;
@@ -88,10 +104,19 @@ public class TableTabsController extends TabController {
 	}
 	
 	@FXML
+	/**
+     * This method changes the value of {@link TableTabsController#selectedDate} to a new chosen date when a new value is chosen by the date picker.
+     * @param event	JavaFX action event
+     */
     void selectTargetDate(ActionEvent event) {
 		this.selectedDate = this.datePicker.getValue();
     }
-	
+	/**
+	 * This method generate an list of data to be put into the table, with the given user inputs of countries and date.
+	 * @param isoCodes		A list of iso codes of the countries that the user has chosen.
+	 * @param targetDate	The date chosen by the user.
+	 * @return				An {@link java.util.ArrayList} of {@link TableData} of the given countries and date.
+	 */
 	protected ArrayList<TableData> generateDataList(ArrayList<String> isoCodes, LocalDate targetDate) {
 		ArrayList<TableData> result = new ArrayList<TableData>();
 		Database db = this.getDatabase();
@@ -102,7 +127,11 @@ public class TableTabsController extends TabController {
 		}
 		return result;
 	}
-	
+	/**
+	 * This method puts data on the table.
+	 * @param data	the list of data to be displayed.
+	 * @param date	the chosen date, used for the title.
+	 */
 	protected void generateTable(ArrayList<TableData> data, LocalDate date) {
 		ObservableList<TableData> oList = FXCollections.observableArrayList(data);
 		this.setTableTitle();
@@ -110,25 +139,45 @@ public class TableTabsController extends TabController {
 		this.dataTable.setItems(oList);
 	}
 	
-	//TODO: Override
-	protected long getTotalDataFromDB(String isoCode, LocalDate targetDate) {
-		return 0;
-	}
+	/**
+	 * This method fetches a value of data of the given location and date from the database.
+	 * Return value is restricted to be that of cumulative number of cases, deaths, and people fully vaccinated.
+	 * @param isoCode		iso code of the chosen location.
+	 * @param targetDate	date of the user's choice.
+	 * @return				a value of data of the given location and date.
+	 */
+	protected abstract long getTotalDataFromDB(String isoCode, LocalDate targetDate);
 	
-	//TODO: Override
-	protected double getRateDataFromDB(String isoCode, LocalDate targetDate) {
-		return 0;
-	}
+	/**
+	 * This method fetches a value of data of the given location and date from the database.
+	 * Return value is restricted to be that of cumulative number of cases per 1M people, deaths per 1M people, and rate of vaccination.
+	 * @param isoCode		iso code of the chosen location
+	 * @param targetDate	date of the user's choice
+	 * @return				a value of the given parameters.
+	 */
+	protected abstract double getRateDataFromDB(String isoCode, LocalDate targetDate);
 	
-	//TODO: Override
-	protected void setTableTitle() {}
+	/**
+	 * This method sets the title of the table according to the type of the tab.
+	 */
+	protected abstract void setTableTitle();
 	
-	//TODO: Override
-	protected TableData getTableData(String iso, long totalData, double rateData) {
-		return null;
-	}
+	/**
+	 * This method constructs a {@link TableData} object with the given parameters.
+	 * Depending of the type of the tab, a percentage sign % may be added to the back of rate data during the construction.
+	 * @param iso		Iso code of the desired location.
+	 * @param totalData	Value for the second column.
+	 * @param rateData	Value for the third column.
+	 * @return			A {@link TableData} object consisting of the above values.
+	 */
+	protected abstract TableData getTableData(String iso, long totalData, double rateData);
 	
 	@Override
+	/**
+	 * {@inheritDoc}
+	 * In this instance, this method generates a list of {@link TableData} given the user's input with {@link TableTabsController#generateDataList(ArrayList, LocalDate)},
+	 * and put them onto the table using {@link TableTabsController#generateTable(ArrayList, LocalDate)}.
+	 */
 	final void handleConfirmSelection(ActionEvent event) {
 		int errorCode = this.handleTableError();
 		if(errorCode != 0)
